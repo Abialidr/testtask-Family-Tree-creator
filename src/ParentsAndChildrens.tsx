9b42bd2baab50ce92992ef9ecb58d4c955d30a0b
Abialidr <luckybelt79@gmail.com>
2024-01-19 14:20:33 +0530

    added changes

--- src/ParentsAndChildrens.tsx
+++ src/ParentsAndChildrens.tsx
@@ -10,31 +10,110 @@ import {
   printChildren,
   printFamily,
 } from './mainFunction';
+import { array, number, object, string } from 'yup';
+
+const familyValidationSchema = object({
+  id: number().required('required'),
+  name: string().required('Required'),
+  gender: string().required('Required'),
+  children: array(number().required('Required')),
+  parents: array(number().required('Required')),
+});
+const familyValidationArrayScheme = array(familyValidationSchema);
+
 const ParentsAndChildrens = () => {
   const [parentsState, setParents] = useState<InterfaceParents>({
     absoluteParents: [],
     famalyHerachy: [],
     completeFamily: [],
   });
-  const [data, setData] = useState(false);
+  const [family, setFamily] = useState(JSON.stringify(familyTree, null, 4));
+  const [loading, setLoading] = useState(false);
+  const [Error, setError] = useState('');
+  const [reRender, setReRender] = useState(false);
   useEffect(() => {
-    const parents: InterfaceParents = {
-      absoluteParents: [],
-      famalyHerachy: [],
-      completeFamily: [],
-    };
-    const relations: InterfaceRelationship[] = [];
-    seperateAbsolute(familyTree, parents);
-    makeLevels(familyTree, parents, findSpouse);
-    makeRelations(familyTree, parents, relations);
-    importRelations(parents, relations);
-    setParents(parents);
-    setData(true);
-  }, []);
-  if (data) {
-    return <div>{printFamily(parentsState, printChildren)}</div>;
-  } else {
-    return <div>LOADING....</div>;
-  }
+    (async () => {
+      try {
+        const parsedFamily = JSON.parse(family);
+        await familyValidationArrayScheme.validate(parsedFamily, {
+          strict: true,
+        });
+
+        const parents: InterfaceParents = {
+          absoluteParents: [],
+          famalyHerachy: [],
+          completeFamily: [],
+        };
+        const relations: InterfaceRelationship[] = [];
+        seperateAbsolute(parsedFamily, parents);
+        makeLevels(parsedFamily, parents, findSpouse);
+        makeRelations(parsedFamily, parents, relations);
+        importRelations(parents, relations);
+        setParents(parents);
+        setLoading(false);
+      } catch (error: any) {
+        setLoading(false);
+        setError(error.message);
+        console.log(`🚀 ~ error:`, error.message);
+        // alert(error);
+      }
+    })();
+  }, [reRender]);
+  return (
+    <div style={{ display: 'flex', gap: '10px' }}>
+      <div
+        style={{
+          borderRight: '1px solid black',
+        }}
+      >
+        <h1>Input</h1>
+        <div
+          style={{
+            width: '400px',
+            textAlign: 'start',
+          }}
+        >
+          <textarea
+            value={family}
+            style={{ height: '100%', display: 'block', width: '300px' }}
+            onChange={(e) => {
+              setFamily(e.target.value);
+            }}
+            rows={50}
+          />
+          <br></br>
+          <button
+            onClick={() => {
+              setLoading(true);
+              setError('');
+              setReRender(!reRender);
+            }}
+          >
+            Re-Render
+          </button>
+          {/* <pre>{`${family}`}</pre> */}
+        </div>
+      </div>
+      <div style={{ flex: 1, wordWrap: 'break-word' }}>
+        {loading ? (
+          <h1>'Loading...'</h1>
+        ) : Error ? (
+          <h1>{Error}</h1>
+        ) : (
+          <>
+            <h1>Output</h1>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              {printFamily(parentsState, printChildren)}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 export default ParentsAndChildrens;
